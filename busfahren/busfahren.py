8e2ee2ef131cac6b3c5e01231869132b807076ee
root <root@h2979442.stratoserver.net>
2022-08-13 16:32:01 +0200

    JS

--- busfahren/busfahren.py
+++ busfahren/busfahren.py
@@ -1,51 +1,53 @@
 #-*- coding:utf-8 -*-
 import random
-# 7 = 7, A = 14
-card_pool = [
-    [[7,False],[8,False],[9,False],[10,False],[11,False],[12,False],[13,False],[14,False]], # Herz
-    [[7,False],[8,False],[9,False],[10,False],[11,False],[12,False],[13,False],[14,False]], # Karo
-    [[7,False],[8,False],[9,False],[10,False],[11,False],[12,False],[13,False],[14,False]], # Schippe
-    [[7,False],[8,False],[9,False],[10,False],[11,False],[12,False],[13,False],[14,False]] # Kreuz
-]
 
-def make_card_pool():
-    # make list of (card_pool)
-    pass
+class Busfahren:
+    def __init__(self) -> None:  
+        self.used_cards_list = list()
+        self.unused_cards_list = list()
+        self.make_card_pool()
 
-def take_random_card_from_pool():
-    global card_pool
-    tf = True
-    while tf:
-        card = random.randint(7-7,14-7) # 7-A
-        pic = random.randint(0,3) # H, P, S, K 
-        if not card_pool[pic][card][1]:
-            card_pool[pic][card][1] = True
-            tf = False
-    return [card, pic]
-def random_cards_for_triangle(y_size=5, min_card=7, max_card=13):
-    global card_pool
-    data = []
-    for i in range(1, y_size):
-        tmp_data = []
-        for ii in range(i):
-            tmp_data.append(take_random_card_from_pool())
-        data.append(tmp_data)
-    return data
-def random_cards_for_player(player_id=None, anz_player_cards=3):
-    global card_pool
-    player_cards = []
-    for i in range(anz_player_cards):
-        player_cards.append(take_random_card_from_pool())
-    return player_cards
-def get_playercards(player_id=None, anz_player_cards=3):
-    # 7 = 1, A = 8
-    return None
-#def make_test_game():
-#    print(random_cards_for_triangle(5))
-#    print("------")
-#    print("Player 1")
-#    print(random_cards_for_player())
-#    print("Player2")
-#    print(random_cards_for_player())##
-#
-#make_test_game()
+    def make_card_pool(self):
+        # initialize used and unused cards 
+        for symbol in ["H", "D", "S", "C"]:           # heart, diamond, spades, club
+            for i in [7, 8, 9, 10, 11, 12, 13, 14]:   # 7 = 7, A = 14
+                self.unused_cards_list.append([i, symbol])  # initialize unused cards
+        self.used_cards_list = list()                 # initialize used cards
+
+    def get_unused_card(self):
+        # get unused card
+        if not len(self.unused_cards_list) > 0:                       # raise exception if there is no unnused card
+            raise BaseException("There is no unnused card")
+        card_pos = random.randint(0, len(self.unused_cards_list) - 1) # choose random unused card
+        self.used_cards_list.append(self.unused_cards_list[card_pos]) # append to used cards
+        self.unused_cards_list.pop(card_pos)                          # remove card from unused cards
+        return self.used_cards_list[-1]                               # return choosen card (last one in the list)
+
+
+    def get_map(self, row_nums=4):
+        # get map that is played
+        # first implemented map is a pyramide
+        map = list()
+        for i in range(row_nums):
+            map.append(list())
+            for _ in range(i + 1):
+                map[-1].append(self.get_unused_card())
+        return map
+
+    def get_player_cards(self, player_cards_num=3):
+        # get cards for player
+        player_cards_list = list()
+        for _ in range(player_cards_num):
+            player_cards_list.append(self.get_unused_card())
+        return player_cards_list
+
+def make_test_game():
+    game = Busfahren()
+    print(game.get_map())
+    print("------")
+    print("Player 1")
+    print(game.get_player_cards())
+    print("Player2")
+    print(game.get_player_cards())
+
+make_test_game()
