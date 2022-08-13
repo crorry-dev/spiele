3f07256aafb08cb096977d2639cf3938c555b0d0
root <root@h2979442.stratoserver.net>
2022-08-13 15:55:42 +0200

    tc

--- busfahren/HTML_Forms.py
+++ busfahren/HTML_Forms.py
@@ -36,27 +36,6 @@ class Form(Form):
     contact_contact = StringField("Ihre Telefonnummer oder Email Adresse", [validators.Length(min=1, max=9999)], render_kw={"style": "width: 75%; font-size: 20px;"})
     contact_text = TextAreaField("Beschreibung Ihrer Anfrage", [validators.Length(min=1, max=9999)], render_kw={"style": "width: 75%; height: 40%; font-size: 20px;"})
 
-    # Zeiterfassung 
-    zeiterfassung_zeit_von = DateField("Von", format="%Y-%m-%d", default=datetime.datetime.now(), render_kw={"style": "width:200px"})
-    zeiterfassung_zeit_von_time = TimeField("", format="%H:%M", default=datetime.datetime.now(), render_kw={"style": "width:200px"})
-    zeiterfassung_zeit_bis = DateField("Bis", format="%Y-%m-%d", default=datetime.datetime.now(), render_kw={"style": "width:200px"})
-    zeiterfassung_zeit_bis_time = TimeField("", format="%H:%M", default=datetime.datetime.now(), render_kw={"style": "width:200px"})
-    zeiterfassung_zeit_job = SelectField("Tätigkeit", choices=["Trainings-Leistung", "Co-Trainings-Leistung", "Wettkampfrichter"], render_kw={"style": "width:200px"})
-
-    # Reisekosten 
-    reisekosten_ort_von = StringField("Start Ort", render_kw={"style": "width:400px; background-color: #ebeced"})
-    reisekosten_ort_bis = StringField("Ziel Ort", render_kw={"style": "width:400px; background-color: #ebeced"})
-    reisekosten_ort_km = StringField("Anzahl Kilometer hin- und zurück als Ganze Zahl angeben", render_kw={"style": "width:100px; background-color: #ebeced"})
-    reisekosten_zeit_von = DateField("Von", format="%Y-%m-%d", default=datetime.datetime.now(), render_kw={"style": "width:200px; background-color: #ebeced"})
-    reisekosten_zeit_bis = DateField("Bis", format="%Y-%m-%d", default=datetime.datetime.now(), render_kw={"style": "width:200px; background-color: #ebeced"})
-    reisekosten_ort_person_1 = StringField("Mitgefahrene Person 1", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_2 = StringField("Mitgefahrene Person 2", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_3 = StringField("Mitgefahrene Person 3", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_4 = StringField("Mitgefahrene Person 4", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_5 = StringField("Mitgefahrene Person 5", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_6 = StringField("Mitgefahrene Person 6", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_7 = StringField("Mitgefahrene Person 7", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_8 = StringField("Mitgefahrene Person 8", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_9 = StringField("Mitgefahrene Person 9", render_kw={"style": "width:50%; background-color: #ebeced"})
-    reisekosten_ort_person_10 = StringField("Mitgefahrene Person 10", render_kw={"style": "width:50%; background-color: #ebeced"})
+    # Busfahren Lobby
+    busfahren_nickname = StringField("Nickname", [validators.Length(min=1, max=9999)], render_kw={"style": "width: 50%; font-size: 20px;"})
     
--- busfahren/app.py
+++ busfahren/app.py
@@ -28,7 +28,7 @@ app.secret_key = "SoftwareDieburg_P!nD@t@_{}".format(random.randint(1000000, 999
 def home():
 	form = HTML_Forms.Form(request.form)
 	json_data = json_db.read()
-	return redirect(url_for("page_busfahren"))
+	return redirect(url_for("page_busfahren_lobby"))
 	# return render_template("home.html", form=form, json_data=json_data)
 # ----------------------------------------------------------------------------------------- #
 @app.route("/login", methods=["GET","POST"])
@@ -116,15 +116,37 @@ def forgot_password():
 def page_busfahren():
 	form = HTML_Forms.Form(request.form)
 	json_data = json_db.read()
-	# waiting for players? 
-	# busfahren.make_card_pool()
+
 	trigangle_cards = busfahren.random_cards_for_triangle()
-	print(trigangle_cards)
-	# for player
 	player_cards = busfahren.random_cards_for_player()
-	print(player_cards)
+
 	return render_template("busfahren.html", form=form, josn_data=json_data, trigangle_cards=trigangle_cards, player_cards=player_cards)
 
+
+@app.route('/busfahren_lobby', methods=["GET", "POST"])
+def page_busfahren_lobby():
+	form = HTML_Forms.Form(request.form)
+	json_data = json_db.read()
+	json_busfahren = json_db.read("busfahren")
+	
+	if "players" in json_busfahren:
+		anz_players = len(json_busfahren["players"])
+	else:
+		anz_players = 0
+
+	if request.method == "POST":
+		if "nickname" not in session:
+			nickname = form.busfahren_nickname.data
+			if nickname != "":
+				session["nickname"] = nickname
+				if "players" not in json_busfahren:
+					json_busfahren["players"] = []
+				json_busfahren["players"].append(nickname)
+
+				json_db.write(json_busfahren, "busfahren")
+		return redirect(url_for("page_busfahren_lobby"))
+
+	return render_template("/busfahren_lobby.html", form=form, josn_data=json_data, anz_players=anz_players)
 
 
 
